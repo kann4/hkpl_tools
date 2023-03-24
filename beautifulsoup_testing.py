b90383767ca0453aba457e3bd9f0b2ac721711dc
kann4 <kann4kann4@gmail.com>
2023-03-24 22:00:33 +0800

    add option to not get book info in get_info strip 0 in url return error msg if url fail add creation date

--- beautifulsoup_testing.py
+++ beautifulsoup_testing.py
@@ -3,7 +3,7 @@ import urllib.error
 from bs4 import BeautifulSoup
 import datetime
 
-def get_info(bib):
+def get_info(bib, get_book_info=True):
     global book, copies
     def get_value(book_info_type, table_found=[False]):
         global table_book
@@ -22,29 +22,31 @@ def get_info(bib):
             return 'N/A'
         return book_info
 
-    url = 'https://webcat.hkpl.gov.hk/lib/item?id=chamo:'+str(bib)+'&fromLocationLink=false&theme=mobile&showAll=true&locale=en'
+    url = 'https://webcat.hkpl.gov.hk/lib/item?id=chamo:'+str(bib).lstrip('0')+'&fromLocationLink=false&theme=mobile&showAll=true&locale=en'
     # response = urllib.request.urlopen(url)
     try:
         response = urllib.request.urlopen(urllib.request.Request(url,headers={'User-Agent': 'Mozilla/5.0'}))
     except urllib.request.HTTPError as e:
         print(e)
-        return
+        return f'Error. "{url}" cannot be reached. Check hkpl web or bib exist.'
 
     htmlbytes = response.read()
     bs = BeautifulSoup(htmlbytes, "html.parser")
     today_date = datetime.date.today()
+    time_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     #book
-    book = []
-    book_title = bs.find('h1', class_='title').string
-    book.append(book_title)
-    
-    items = ['Author', 'Bib ID', 'Call Number', 'Physical Description', 'Place of Publication', 'Publisher',
-    'Year', 'Series Title', 'Subject', 'Added Author', 'Standard No.', 'Language']
-    for item in items:
-        book.append(get_value(item))
-    book.append(str(today_date))
-    book.append(str(today_date))
-    tuple(book)
+    if get_book_info==True:
+        book = []
+        book_title = bs.find('h1', class_='title').string
+        book.append(book_title)
+        
+        items = ['Author', 'Bib ID', 'Call Number', 'Physical Description', 'Place of Publication', 'Publisher',
+        'Year', 'Series Title', 'Subject', 'Added Author', 'Standard No.', 'Language']
+        for item in items:
+            book.append(get_value(item))
+        book.append(str(today_date))
+        book.append(str(today_date))
+        tuple(book)
     #copies
     table_copies = bs.find_all('form')[2].tbody
     trs = table_copies.find_all('tr')[:-1]
@@ -56,11 +58,13 @@ def get_info(bib):
         library = divs[0].string[4:].rstrip()
         status = divs[3].string
         collection = divs[4].string
-        creation_date =  last_update_date = str(today_date)
+        creation_date =  last_update_date = str(time_now)
         copies[i] = [library,status,collection,creation_date,last_update_date]    
 
-    return (book,copies)
-
+    if get_book_info==True:
+        return (book,copies)
+    else:
+        return copies
 # print(get_info(003563388))
 # print(len(copies))    
 
