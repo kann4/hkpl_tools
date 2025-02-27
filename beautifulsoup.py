7a0c996ff7b3c06497ef03ccdfae914ec661ec99
Victor Lo <victorlo@ubizense.com>
2025-02-27 21:58:50 +0800

    refactor get_value in beautifulsoup.py

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -3,24 +3,25 @@ import urllib.error
 from bs4 import BeautifulSoup
 import datetime
 
+def get_value(table_book, book_info_type, table_found=[False]):
+    # global table_book
+    # if table_found == [False]:
+    #     table_book = bs.find('div', class_='itemFields').table          
+    #     table_found[0] = True
+    td = table_book.find('td', string=book_info_type)     #tdre where item name is placed
+    if td != None:
+        book_info = td.find_next_sibling('td').string      #next td is the value of the item
+        if book_info == None:                               #sometimes the info has > 1 line. td is empty. Those info are placed in the divs inside that td
+            book_info = ''
+            divs = td.find_next_sibling('td').find_all('div')
+            for div in divs:
+                book_info = book_info + div.string + '\n'
+    else:
+        return 'N/A'
+    return book_info
+
 def get_info(bib, get_book_info=True):
     global book, copies
-    def get_value(book_info_type, table_found=[False]):
-        global table_book
-        if table_found == [False]:
-            table_book = bs.find('div', class_='itemFields').table          
-            table_found[0] = True
-        td = table_book.find('td', string=book_info_type)     #td where item name is placed
-        if td != None:
-            book_info = td.find_next_sibling('td').string      #next td is the value of the item
-            if book_info == None:                               #sometimes the info has > 1 line. td is empty. Those info are placed in the divs inside that td
-                book_info = ''
-                divs = td.find_next_sibling('td').find_all('div')
-                for div in divs:
-                    book_info = book_info + div.string + '\n'
-        else:
-            return 'N/A'
-        return book_info
 
     url = 'https://webcat.hkpl.gov.hk/lib/item?id=chamo:'+str(bib).lstrip('0')+'&fromLocationLink=false&theme=mobile&showAll=true&locale=en'
     # response = urllib.request.urlopen(url)
@@ -42,8 +43,10 @@ def get_info(bib, get_book_info=True):
         
         items = ['Author', 'Bib ID', 'Call Number', 'Physical Description', 'Place of Publication', 'Publisher',
         'Year', 'Series Title', 'Subject', 'Added Author', 'Standard No.', 'Language']
+
+        table_book = bs.find('div', class_='itemFields').table
         for item in items:
-            book.append(get_value(item))
+            book.append(get_value(table_book, item))
         book.append(str(today_date))
         book.append(str(today_date))
         tuple(book)
